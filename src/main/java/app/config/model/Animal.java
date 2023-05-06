295bb6dc1a9f9cf856a436b897f064aadf483631
seriousgraf <grafnotstupid@gmail.com>
2023-05-06 14:19:19 +0300

    Initial commit

---
+++ src/main/java/app/config/model/Animal.java
@@ -0,0 +1,4 @@
+package app.config.model;
+
+public abstract class Animal {
+}
---
+++ src/main/java/app/config/model/Timer.java
@@ -0,0 +1,13 @@
+package app.config.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer {
+
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
